dd0ee098387597746faaece0f16d30b0daee92c0
Jan Christophersen <jan@ruken.pw>
2017-05-12 23:55:50 +0200

    Add Category/Categories and refactor API

---
+++ categories.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// Categories is an array of Category
+type Categories []Category
+
+// Category represents a Category with Entries
+type Category struct {
+	idCounter int
+	ID        int
+	Name      string
+	Entries   Entries
+}
+
+// NewCategory creates a new Category
+func NewCategory(id int, name string) Category {
+	return Category{
+		0,
+		id,
+		name,
+		make(Entries, 0),
+	}
+}
+
+// ToJSON marshals the Categories into a JSON string
+func (c *Categories) ToJSON() []byte {
+	res, _ := json.Marshal(c)
+
+	return res
+}
+
+// ToJSON marshals the Category into a JSON string
+func (c *Category) ToJSON() []byte {
+	res, _ := json.Marshal(c)
+
+	return res
+}
+
+// NewEntry creates a new Entry inside the Category
+func (c *Category) NewEntry(title string) Entry {
+	id := c.idCounter
+	c.idCounter++
+
+	e := NewEntry(id, title)
+
+	c.Entries = append(c.Entries, e)
+
+	return e
+}
+
+// GetEntryByID returns the Entry by its ID
+func (c *Category) GetEntryByID(id int) *Entry {
+	for _, e := range c.Entries {
+		if e.ID == id {
+			return &e
+		}
+	}
+	return nil
+}
+
+// RemoveEntryByID removed the Entry by its ID
+func (c *Category) RemoveEntryByID(id int) error {
+	pos := -1
+
+	for idx, e := range c.Entries {
+		if e.ID == id {
+			pos = idx
+			break
+		}
+	}
+
+	if pos == -1 {
+		return errors.New("Not found")
+	}
+
+	c.Entries = append(c.Entries[:pos], c.Entries[pos+1:]...)
+
+	return nil
+}
+
+func apiGetEntries(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id, err := strconv.Atoi(p.ByName("category"))
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	c := standup.GetCategoryByID(id)
+	if c == nil {
+		w.WriteHeader(404)
+		w.Write([]byte("Category not found"))
+		return
+	}
+
+	w.Write(c.Entries.ToJSON())
+}
+
+func apiCreateEntry(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id, err := strconv.Atoi(p.ByName("category"))
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	c := standup.GetCategoryByID(id)
+	if c == nil {
+		w.WriteHeader(404)
+		w.Write([]byte("Category not found"))
+		return
+	}
+
+	var entry Entry
+
+	if err := getJSON(r, &entry); err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	entry = c.NewEntry(entry.Title)
+
+	w.Write(entry.ToJSON())
+}
+
+func apiGetEntry(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id, err := strconv.Atoi(p.ByName("category"))
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	c := standup.GetCategoryByID(id)
+	if c == nil {
+		w.WriteHeader(404)
+		w.Write([]byte("Category not found"))
+		return
+	}
+
+	eID, err := strconv.Atoi(p.ByName("entry"))
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	e := c.GetEntryByID(eID)
+
+	w.Write(e.ToJSON())
+}
+
+func apiUpdateEntry(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id, err := strconv.Atoi(p.ByName("category"))
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	c := standup.GetCategoryByID(id)
+	if c == nil {
+		w.WriteHeader(404)
+		w.Write([]byte("Category not found"))
+		return
+	}
+
+	eID, err := strconv.Atoi(p.ByName("entry"))
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	e := c.GetEntryByID(eID)
+
+	if e == nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	var entry Entry
+
+	if err := getJSON(r, &entry); err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	e.Title = entry.Title
+	e.Votes = entry.Votes
+
+	w.Write(e.ToJSON())
+}
+
+func apiDeleteEntry(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id, err := strconv.Atoi(p.ByName("category"))
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	c := standup.GetCategoryByID(id)
+	if c == nil {
+		w.WriteHeader(404)
+		w.Write([]byte("Category not found"))
+		return
+	}
+
+	eID, err := strconv.Atoi(p.ByName("entry"))
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	if c.RemoveEntryByID(eID) != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.WriteHeader(200)
+}
--- entries.go
+++ entries.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // Entries represent an Array of Entry
@@ -14,10 +10,18 @@ type Entries []Entry
 // Entry consists of a Category and Title for the Standup
 // you can also vote on Entries in the future. Hopefully.
 type Entry struct {
-	ID       int
-	Category string
-	Title    string
-	Votes    uint32
+	ID    int
+	Title string
+	Votes uint32
+}
+
+// NewEntry creates a new Entry
+func NewEntry(id int, title string) Entry {
+	return Entry{
+		id,
+		title,
+		0,
+	}
 }
 
 // ToJSON marshals the Entries into a JSON string
@@ -32,86 +36,4 @@ func (e *Entry) ToJSON() []byte {
 	res, _ := json.Marshal(e)
 
 	return res
-}
-
-func apiGetEntries(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	w.Write(standup.Entries.ToJSON())
-}
-
-func apiCreateEntry(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	var entry Entry
-
-	if err := getJSON(r, &entry); err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	entry = standup.NewEntry(entry.Category, entry.Title)
-
-	w.Write(entry.ToJSON())
-}
-
-func apiUpdateEntry(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	id, err := strconv.Atoi(p.ByName("id"))
-
-	if err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	pos := -1
-
-	for idx, entry := range standup.Entries {
-		if entry.ID == id {
-			pos = idx
-			break
-		}
-	}
-
-	if pos == -1 {
-		w.WriteHeader(404)
-		return
-	}
-
-	var entry Entry
-
-	if err := getJSON(r, &entry); err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	existing := &standup.Entries[pos]
-
-	existing.Category = entry.Category
-	existing.Title = entry.Title
-	existing.Votes = entry.Votes
-
-	w.Write(existing.ToJSON())
-}
-
-func apiDeleteEntry(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	id, err := strconv.Atoi(p.ByName("id"))
-
-	if err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	pos := -1
-
-	for idx, entry := range standup.Entries {
-		if entry.ID == id {
-			pos = idx
-			break
-		}
-	}
-
-	if pos == -1 {
-		w.WriteHeader(404)
-		return
-	}
-
-	standup.Entries = append(standup.Entries[:pos], standup.Entries[pos+1:]...)
-
-	w.WriteHeader(200)
 }
