9623cacc37f1a4882a67be81033a944c07d58177
Jan Christophersen <jan@ruken.pw>
2017-05-13 13:10:06 +0200

    Started testing

---
+++ standup_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestGetNextDailyOnMonday checks that at 7am UTC, the next daily is still on the same day
+func TestGetNextDailyOnMonday(t *testing.T) {
+	now := time.Date(2017, 01, 02, 07, 00, 00, 00, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 2, next.Day())
+	assert.Equal(t, 9, next.Hour())
+	assert.Equal(t, 20, next.Minute())
+}
+
+// TestGetNextDailyOnLateMonday checks that at 2pm UTC, the next daily is on the next day
+func TestGetNextDailyOnLateMonday(t *testing.T) {
+	now := time.Date(2017, 01, 02, 14, 00, 00, 00, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 3, next.Day())
+	assert.Equal(t, 8, next.Hour())
+	assert.Equal(t, 10, next.Minute())
+}
