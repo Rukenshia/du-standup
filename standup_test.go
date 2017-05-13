abe75ece0b9b196403f8492be249dbadc7e2a914
Jan Christophersen <jan@ruken.pw>
2017-05-13 13:15:40 +0200

    Test all Weekdays for getNextDaily

--- standup_test.go
+++ standup_test.go
@@ -9,7 +9,7 @@ import (
 
 // TestGetNextDailyOnMonday checks that at 7am UTC, the next daily is still on the same day
 func TestGetNextDailyOnMonday(t *testing.T) {
-	now := time.Date(2017, 01, 02, 07, 00, 00, 00, time.UTC)
+	now := time.Date(2017, 1, 2, 7, 0, 0, 0, time.UTC)
 
 	next := getNextDaily(now)
 
@@ -20,7 +20,7 @@ func TestGetNextDailyOnMonday(t *testing.T) {
 
 // TestGetNextDailyOnLateMonday checks that at 2pm UTC, the next daily is on the next day
 func TestGetNextDailyOnLateMonday(t *testing.T) {
-	now := time.Date(2017, 01, 02, 14, 00, 00, 00, time.UTC)
+	now := time.Date(2017, 1, 2, 14, 0, 0, 0, time.UTC)
 
 	next := getNextDaily(now)
 
@@ -31,7 +31,7 @@ func TestGetNextDailyOnLateMonday(t *testing.T) {
 
 // TestGetNextDailyOnTuesday checks that at 7am UTC, the next daily is still on the same day
 func TestGetNextDailyOnTuesday(t *testing.T) {
-	now := time.Date(2017, 01, 03, 07, 00, 00, 00, time.UTC)
+	now := time.Date(2017, 1, 3, 7, 0, 0, 0, time.UTC)
 
 	next := getNextDaily(now)
 
@@ -42,11 +42,99 @@ func TestGetNextDailyOnTuesday(t *testing.T) {
 
 // TestGetNextDailyOnLateTuesday checks that at 2pm UTC, the next daily is on the next day
 func TestGetNextDailyOnLateTuesday(t *testing.T) {
-	now := time.Date(2017, 01, 03, 14, 00, 00, 00, time.UTC)
+	now := time.Date(2017, 1, 3, 14, 0, 0, 0, time.UTC)
 
 	next := getNextDaily(now)
 
 	assert.Equal(t, 4, next.Day())
 	assert.Equal(t, 8, next.Hour())
 	assert.Equal(t, 10, next.Minute())
+}
+
+// TestGetNextDailyOnWednesday checks that at 7am UTC, the next daily is still on the same day
+func TestGetNextDailyOnWednesday(t *testing.T) {
+	now := time.Date(2017, 1, 4, 7, 0, 0, 0, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 4, next.Day())
+	assert.Equal(t, 8, next.Hour())
+	assert.Equal(t, 10, next.Minute())
+}
+
+// TestGetNextDailyOnLateWednesday checks that at 2pm UTC, the next daily is on the next day
+func TestGetNextDailyOnLateWednesday(t *testing.T) {
+	now := time.Date(2017, 1, 4, 14, 0, 0, 0, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 5, next.Day())
+	assert.Equal(t, 8, next.Hour())
+	assert.Equal(t, 10, next.Minute())
+}
+
+// TestGetNextDailyOnThursday checks that at 7am UTC, the next daily is still on the same day
+func TestGetNextDailyOnThursday(t *testing.T) {
+	now := time.Date(2017, 1, 5, 7, 0, 0, 0, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 5, next.Day())
+	assert.Equal(t, 8, next.Hour())
+	assert.Equal(t, 10, next.Minute())
+}
+
+// TestGetNextDailyOnLateThursday checks that at 2pm UTC, the next daily is on the next day
+func TestGetNextDailyOnLateThursday(t *testing.T) {
+	now := time.Date(2017, 1, 5, 14, 0, 0, 0, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 6, next.Day())
+	assert.Equal(t, 8, next.Hour())
+	assert.Equal(t, 10, next.Minute())
+}
+
+// TestGetNextDailyOnFriday checks that at 7am UTC, the next daily is still on the same day
+func TestGetNextDailyOnFriday(t *testing.T) {
+	now := time.Date(2017, 1, 6, 7, 0, 0, 0, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 6, next.Day())
+	assert.Equal(t, 8, next.Hour())
+	assert.Equal(t, 10, next.Minute())
+}
+
+// TestGetNextDailyOnLateFriday checks that at 2pm UTC, the next daily is on monday
+func TestGetNextDailyOnLateFriday(t *testing.T) {
+	now := time.Date(2017, 1, 6, 14, 0, 0, 0, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 9, next.Day())
+	assert.Equal(t, 9, next.Hour())
+	assert.Equal(t, 20, next.Minute())
+}
+
+// TestGetNextDailyOnSaturday checks that at 2pm UTC, the next daily is on monday
+func TestGetNextDailyOnSaturday(t *testing.T) {
+	now := time.Date(2017, 1, 7, 14, 0, 0, 0, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 9, next.Day())
+	assert.Equal(t, 9, next.Hour())
+	assert.Equal(t, 20, next.Minute())
+}
+
+// TestGetNextDailyOnSunday checks that at 2pm UTC, the next daily is on monday
+func TestGetNextDailyOnSunday(t *testing.T) {
+	now := time.Date(2017, 1, 8, 14, 0, 0, 0, time.UTC)
+
+	next := getNextDaily(now)
+
+	assert.Equal(t, 9, next.Day())
+	assert.Equal(t, 9, next.Hour())
+	assert.Equal(t, 20, next.Minute())
 }
